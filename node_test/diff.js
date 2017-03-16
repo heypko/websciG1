794c31dacd03112ad67c7dd271520a8c4e32492d
Peter <kopeterj@gmail.com>
2017-03-16 17:54:11 -0400

    Tests for parsing text from ecode statute repository, diff check with JSON files.

---
+++ node_test/diff.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var jsdiff = require('diff');
+var fs = require('fs');
+var htmlToJson = require('html-to-json');
+require('colors')
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/diff.html');
+})
+
+app.use(express.static(__dirname));
+
+// Parse text from ecode360
+var statParser = htmlToJson.createParser(['a.titleLink,.para', {
+  'text': function ($a) {
+    return $a.text();
+  },
+}]);
+
+statParser.request('http://ecode360.com/print/GL1564?guid=11768279&children=true').done(function (stats) {
+  console.log(stats);
+});
+
+// Demo for text comparison
+var json1 = JSON.parse(fs.readFileSync('filecmp1.json'));
+var json2 = JSON.parse(fs.readFileSync('filecmp2.json'));
+
+console.log(json1);
+console.log(json2);
+
+diff = jsdiff.diffJson(json1, json2);
+
+diff.forEach(function(part){
+  // green for additions, red for deletions
+  // grey for common parts
+  var color = part.added ? 'green' :
+  part.removed ? 'red' : 'grey';
+  console.log(part.value[color]);
+  // console.log("Added: " + part.added);
+  // console.log("Removed: " + part.removed);
+});
+
+server.listen(3000, function () {
+  console.log('Server up on *:3000');
+})
